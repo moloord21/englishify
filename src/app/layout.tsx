845e2109a2f146ed885b0ec482685ce920ed6f84
محمود فوزى احمد عجيز <UG_31117165@pharm.tanta.edu.eg>
2025-01-25 00:58:49 +0200

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin', 'arabic'] })
+
+export const metadata = {
+  title: 'Englishify',
+  description: 'منصة لتعلم اللغة الإنجليزية',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
