2ab84abee8ea67289ae848f9ab8c3dc39157bf59
محمود فوزى احمد عجيز <UG_31117165@pharm.tanta.edu.eg>
2025-01-25 01:43:00 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from '@/contexts/AuthContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   )
 }
